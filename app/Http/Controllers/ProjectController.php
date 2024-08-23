60b9458f0515ec86f262c1ab3d7b30f3a7b3d897
rky-ke <ronaldyegon15@gmail.com>
2024-08-23 07:36:58 +0300

    Updated Front End to fetch projects, filter and sort them

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -16,14 +16,46 @@ class ProjectController extends Controller
     {
         $query = Project::query();
 
-        $projects = $query->paginate(10)->OnEachSide(1);
+        $sortField = request('sort_field', 'id');
+        $sortDirection = request('sort_direction', 'desc');
+
+        if (request('name')) {
+            $query->where('name', 'like', '%'.request('name').'%');
+        }
+
+        if (request('status')) {
+            $query->where('status', request('status'));
+        }
+
+        $projects = $query->orderBy($sortField, $sortDirection)
+            ->paginate(10)
+            ->OnEachSide(1);
         
         return inertia('Project/Index', [
-            'projects' => ProjectResource::collection(Project::all()),
+            'projects' => ProjectResource::collection($projects),
+            'queryParams' => request()->query() ?: null,
             
         ]);
     }
 
+    // public function index()
+    // {
+    //     $query = Project::query();
+
+    //     if (request("name")) {
+    //         $query->where("name", "like", "%" . request("name") . "%");
+    //     }
+    //     if (request("status")) {
+    //         $query->where("status", request("status"));
+    //     }
+
+    //     $projects = $query->paginate(10)->onEachSide(1);
+
+    //     return inertia("Project/Index", [
+    //         "projects" => ProjectResource::collection($projects),
+    //         'queryParams' => request()->query() ?: null,
+    //     ]);
+    // }
     /**
      * Show the form for creating a new resource.
      */
